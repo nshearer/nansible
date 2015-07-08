a1ecce0848457a6d9dffd9bdd70a74b92ec567ab
Nathan Shearer <shearern@gmail.com>
2015-07-08 14:49:38 -0700

    Added doc to top of nan command

--- src/nansible/tpl/__init__.py
+++ src/nansible/tpl/__init__.py
@@ -3,9 +3,11 @@
 from TemplateTaskWizard import TemplateTaskWizard
 from CopyTaskWizard import CopyTaskWizard
 from MkdirTaskWizard import MkdirTaskWizard
+from InstallPackageTaskWizard import InstallPackageTaskWizard
 
 ALL_TEMPLATES = {
     TemplateTaskWizard.TASK_NAME: TemplateTaskWizard,
     CopyTaskWizard.TASK_NAME: CopyTaskWizard,
     MkdirTaskWizard.TASK_NAME: MkdirTaskWizard,
+    InstallPackageTaskWizard.TASK_NAME: InstallPackageTaskWizard,
     }
